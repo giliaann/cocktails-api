149cf4183dc8e6cbf332f598a29c8558ed33496e
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-03-15 19:11:25 +0100

    Setup Postgres db with Docker, configured TypeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CocktailsModule } from './cocktails/cocktails.module';
 import { IngredientsModule } from './ingredients/ingredients.module';
 
 @Module({
-  imports: [CocktailsModule, IngredientsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'cocktails_database',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    CocktailsModule, 
+    IngredientsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
