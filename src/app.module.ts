786f00adea191ccb1bcd7848d17b40fa11323191
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-03-15 11:02:42 +0100

    Initial commit, basic REST API generated

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CocktailsModule } from './cocktails/cocktails.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
+
+@Module({
+  imports: [CocktailsModule, IngredientsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
