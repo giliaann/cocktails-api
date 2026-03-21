68bd7301f009ee1bb8575c5299cce469cbe0e25d
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-03-21 15:52:37 +0100

    add basic Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cocktails API')
+    .setDescription('Documentation for managing and creating cocktails and ingredients ')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(document));
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
